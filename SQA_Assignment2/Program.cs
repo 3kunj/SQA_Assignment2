01dbe8782603e0e34676cd6bbaabac79a74ecc2b
Kunj Jagdishbhai Tandel <Ktandel9021@conestogac.on.ca>
2019-06-19 19:50:31 -0400

    menu Created

--- SQA_Assignment2/Program.cs
+++ SQA_Assignment2/Program.cs
@@ -10,6 +10,104 @@ namespace SQA_Assignment2
     {
         static void Main(string[] args)
         {
+            //define varibale for input
+            string value1 = "";
+            string value2 = "";
+            string value3 = "";
+            string choice = "";
+            int select = 0;
+            //for dimensions 
+            int line1, line2, line3;
+
+
+
+            do
+            {
+                Console.WriteLine("1. Enter triangle dimensions");
+                Console.WriteLine("2. Exit");
+
+                choice = Console.ReadLine();
+
+                if (Int32.TryParse(choice, out int c) || c > 0)
+                {
+                    select = c;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+               
+
+                switch (select)
+                {
+                    case 1:
+                        do
+                        {
+                            Console.WriteLine("Please Enter Dimension 1:");
+                            value1 = Console.ReadLine();
+
+                            if (Int32.TryParse(value1, out int l) || l > 0)
+                            {
+                                line1 = l;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid dimensions. Please Enter positive integer value. Zero is not acceptable.");
+                            }
+                        }
+                        while (!Int32.TryParse(value1, out int l1) || !(l1 > 0));
+
+                        do
+                        {
+                            Console.WriteLine("Please Enter Dimension 2:");
+                            value1 = Console.ReadLine();
+
+                            if (Int32.TryParse(value1, out int l) || l > 0)
+                            {
+                                line2 = l;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid dimensions. Please Enter positive integer value. Zero is not acceptable.");
+                            }
+                        }
+                        while (!Int32.TryParse(value1, out int l1) || !(l1 > 0));
+
+                        do
+                        {
+                            Console.WriteLine("Please Enter Dimension 3:");
+                            value1 = Console.ReadLine();
+
+                            if (Int32.TryParse(value1, out int l) || l > 0)
+                            {
+                                line3 = l;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid dimensions. Please Enter positive integer value. Zero is not acceptable.");
+                            }
+                        }
+                        while (!Int32.TryParse(value1, out int l1) || !(l1 > 0));
+
+
+
+
+                        break;
+                    
+                    case 2:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+
+            }
+            while (!(select == 2));
+
+
+
+
+
         }
     }
 }
